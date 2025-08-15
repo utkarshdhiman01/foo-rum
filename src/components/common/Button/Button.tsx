d64f024b9415932ec0cfe67cbb7b9b06b9b4a38c
Utkarsh Dhiman <utkarsh.dhiman@brevo.com>
2025-08-15 20:50:14 +0530

    add: button component

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,43 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "active" | "danger" | "gamma";
+  rightIcon?: React.ReactNode;
+  iconOnly?: boolean;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  variant,
+  rightIcon,
+  iconOnly,
+  children,
+  className,
+  ...props
+}) => {
+  const baseStyles =
+    "inline-flex items-center justify-center gap-2 rounded-md text-sm transition-colors min-w-8 hover:bg-stone-50 cursor-pointer hover:shadow mx-0 text-base";
+
+  const variantStyles = variant
+    ? {
+        active: "bg-white text-slate-900 shadow-sm",
+        danger: "bg-red-100 text-white hover:bg-red-600",
+        gamma: "bg-transparent",
+      }[variant]
+    : "";
+
+  return (
+    <button
+      className={`
+        ${iconOnly ? "p-2.5" : "p-2"}
+        ${baseStyles}
+        ${variantStyles}
+        ${className || ""}
+      `}
+      {...props}
+    >
+      {children}
+      {!iconOnly && rightIcon}
+    </button>
+  );
+};
+
+export default Button;
