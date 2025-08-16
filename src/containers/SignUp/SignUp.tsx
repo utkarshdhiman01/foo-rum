c840801f3e1d22de6bdcec18f0adad86e5c399c5
Utkarsh Dhiman <utkarsh.dhiman@brevo.com>
2025-08-16 17:16:41 +0530

    refactor: sign up flow

--- src/containers/SignUp/SignUp.tsx
+++ src/containers/SignUp/SignUp.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Block from "../../components/Block";
 import Input from "../../components/Input";
 import SignUpIcon from "./sign-up.svg?react";
+import { useNavigate } from "react-router";
+import { useUser } from "../../store/User";
 
 const SignUp: React.FC = () => {
   const [username, setEmail] = useState("");
@@ -13,47 +15,20 @@ const SignUp: React.FC = () => {
     repeatPassword?: string;
   }>({});
 
-  const validateForm = () => {
-    const newErrors: {
-      username?: string;
-      password?: string;
-      repeatPassword?: string;
-    } = {};
+  const navigate = useNavigate();
+  const { user, setUser } = useUser();
 
-    // Email or username validation
-    if (!username) {
-      newErrors.username = "Email/username is required";
-    } else if (
-      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$|^[a-zA-Z0-9_-]{3,16}$/.test(
-        username
-      )
-    ) {
-      newErrors.username = "Please enter a valid email address or username";
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+      return;
     }
-
-    // Password validation
-    if (!password) {
-      newErrors.password = "Password is required";
-    } else if (password.length < 8) {
-      newErrors.password = "Password must be at least 8 characters long";
-    }
-
-    // Repeat password validation
-    if (!repeatPassword) {
-      newErrors.repeatPassword = "Please confirm your password";
-    } else if (password !== repeatPassword) {
-      newErrors.repeatPassword = "Passwords do not match";
-    }
-
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
-  };
+  }, [user]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (validateForm()) {
-      // TODO: Handle sign up logic here
-      console.log("Form is valid");
+    if (validateForm(username, password, repeatPassword, setErrors)) {
+      setUser({ username });
     }
   };
 
@@ -123,3 +98,50 @@ const SignUp: React.FC = () => {
 };
 
 export default SignUp;
+
+function validateForm(
+  username: string,
+  password: string,
+  repeatPassword: string,
+  setErrors: React.Dispatch<
+    React.SetStateAction<{
+      username?: string;
+      password?: string;
+      repeatPassword?: string;
+    }>
+  >
+) {
+  const newErrors: {
+    username?: string;
+    password?: string;
+    repeatPassword?: string;
+  } = {};
+
+  // Email or username validation
+  if (!username) {
+    newErrors.username = "Email/username is required";
+  } else if (
+    !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$|^[a-zA-Z0-9_-]{3,16}$/.test(
+      username
+    )
+  ) {
+    newErrors.username = "Please enter a valid email address or username";
+  }
+
+  // Password validation
+  if (!password) {
+    newErrors.password = "Password is required";
+  } else if (password.length < 8) {
+    newErrors.password = "Password must be at least 8 characters long";
+  }
+
+  // Repeat password validation
+  if (!repeatPassword) {
+    newErrors.repeatPassword = "Please confirm your password";
+  } else if (password !== repeatPassword) {
+    newErrors.repeatPassword = "Passwords do not match";
+  }
+
+  setErrors(newErrors);
+  return Object.keys(newErrors).length === 0;
+}
